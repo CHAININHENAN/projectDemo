ef2d5fe63164f3a09eca567e4eaf35b457590a04
zhaopanfeng <zhaopanfeng@vandream.com>
2019-09-21 14:59:58 +0800

    我的测试项目

---
+++ projectDemo/src/service/java8_1.java
@@ -0,0 +1,11 @@
+package service;
+
+/**
+ * @author zhaopanfeng
+ * @date 2019/9/21 14:50
+ */
+public class java8_1 {
+    public static void main(String[] args) {
+        System.out.println("java8");
+    }
+}
