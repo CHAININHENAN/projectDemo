d50c08f6f301074e6d9ba007c47b030f38698153
zhaopanfeng <zhaopanfeng@vandream.com>
2019-09-23 11:26:38 +0800

    T-003 我的测试项目2

---
+++ projectDemo/src/main/service/java8_2.java
@@ -0,0 +1,13 @@
+package main.service;
+
+
+/**
+ * @author zhaopanfeng
+ * @date 2019/9/23 11:25
+ */
+public class java8_2 {
+    public static void main(String[] args) {
+        System.out.println("23");
+    }
+
+}
